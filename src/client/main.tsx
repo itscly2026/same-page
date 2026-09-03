b6eca9afeb33108ed2ed6e9ce523e70889de1a50
siahuat0727 <siahuat0727@siahuat0727deMac-mini.local>
2026-09-03 10:23:45 +0800

    chore: expose sanitized drive cache diagnostics (#73 #78)

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import { buildId } from "../shared/build";
 import { App } from "./app";
 import { getLoadingPerformanceSnapshot } from "./performance/loading-performance";
+import { getDriveLibraryCacheDiagnostics } from "./score-library/drive-library-cache";
 import "./styles.css";
 
 const root = document.getElementById("root");
@@ -17,6 +18,7 @@ Object.defineProperty(window, "__SAME_PAGE_DIAGNOSTICS__", {
   configurable: false,
   value: Object.freeze({
     buildId,
+    driveLibraryCache: getDriveLibraryCacheDiagnostics,
     loadingPerformance: getLoadingPerformanceSnapshot,
   }),
   writable: false,
